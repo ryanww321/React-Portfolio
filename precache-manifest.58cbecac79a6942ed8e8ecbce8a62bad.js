56a188b65b5019cbf5619b54f8a4fdea87b8f1e1
Ryan Whitcomb <ryanww321@gmail.com>
2020-03-25 16:48:48 -0500

    Updates

---
+++ precache-manifest.58cbecac79a6942ed8e8ecbce8a62bad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba458f4ba9708cd38a583775ee013423",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "85de7e7142d93e598c93",
+    "url": "/React-Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2ed38275f64c5f70cdc",
+    "url": "/React-Portfolio/static/js/2.a4bd3829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.a4bd3829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85de7e7142d93e598c93",
+    "url": "/React-Portfolio/static/js/main.a88d8528.chunk.js"
+  },
+  {
+    "revision": "b1cad2d636aa2549d2e5",
+    "url": "/React-Portfolio/static/js/runtime-main.bf13817d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
