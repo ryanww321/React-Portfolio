5d723aaee373db3c8c26319af57b5c6154ecdcf6
Ryan Whitcomb <ryanww321@gmail.com>
2020-03-27 09:28:17 -0500

    fixed errors on portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Portfolio() {
     return (
-        <h2>Portfolio</h2>
         <div class="container">
+            <h2>Portfolio</h2>
             <div class="row">
                 <div class="col-lg-4 col-sm-6 col-xs-12">
                     <div class="card card-outline mb-4 mt-4">
                         <img class="card-img-top" src="./Assets/password.png" alt="password generator" />
                         <div class="card-body"><a href="https://ryanww321.github.io/Password-Generator/"
-                            target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block">Password
+                            target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-block">Password
                                 Generator</button></a>
                         </div>
                     </div>
@@ -17,7 +17,7 @@ function Portfolio() {
                 <div class="col-lg-4 col-sm-6 col-xs-12">
                     <div class="card card-outline mb-4 mt-4">
                         <img class="card-img-top" src="./Assets/code-quiz.png" alt="code quiz" />
-                        <div class="card-body"><a href="https://ryanww321.github.io/Code-Quiz-2.0/" target="_blank"><button
+                        <div class="card-body"><a href="https://ryanww321.github.io/Code-Quiz-2.0/" target="_blank" rel="noopener noreferrer"><button
                             type="button" class="btn btn-primary btn-lg btn-block">Code
                                 Quiz</button></a>
                         </div>
@@ -28,7 +28,7 @@ function Portfolio() {
                 <div class="col-lg-4 col-sm-6 col-xs-12">
                     <div class="card card-outline mb-4">
                         <img class="card-img-top" src="./Assets/scheduler.png" alt="day planner" />
-                        <div class="card-body"><a href="https://ryanww321.github.io/Day-Planner/" target="_blank"><button
+                        <div class="card-body"><a href="https://ryanww321.github.io/Day-Planner/" target="_blank" rel="noopener noreferrer"><button
                             type="button" class="btn btn-primary btn-lg btn-block">Work Day
                                 Scheduler</button></a>
                         </div>
@@ -38,7 +38,7 @@ function Portfolio() {
                     <div class="card card-outline mb-4">
                         <img class="card-img-top" src="./Assets/weather-dashboard.png" alt="weather dashboard" />
                         <div class="card-body"><a href="https://ryanww321.github.io/Weather-Dashboard/"
-                            target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block">Weather
+                            target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-block">Weather
                                 Dashboard</button></a>
                         </div>
                     </div>
@@ -48,14 +48,14 @@ function Portfolio() {
             <div class="col-lg-4 col-sm-6 col-xs-12">
                 <div class="card card-outline mb-4">
                     <img class="card-img-top" src="./Assets/notetaker.png" alt="day planner" />
-                    <div class="card-body"><a href="https://powerful-citadel-70509.herokuapp.com/" target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block">Note Taker</button></a>
+                    <div class="card-body"><a href="https://powerful-citadel-70509.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-block">Note Taker</button></a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-sm-6 col-xs-12">
                 <div class="card card-outline mb-4">
                     <img class="card-img-top" src="./Assets/eatdaburger.png" alt="weather dashboard" />
-                    <div class="card-body"><a href="https://quiet-everglades-33030.herokuapp.com/" target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block">Eat-da-burger</button></a>
+                    <div class="card-body"><a href="https://quiet-everglades-33030.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-block">Eat-da-burger</button></a>
                     </div>
                 </div>
             </div>
@@ -64,10 +64,11 @@ function Portfolio() {
                 <div class="col-lg-4 col-sm-6 col-xs-12">
                     <div class="card card-outline mb-4">
                         <img class="card-img-top" src="./Assets/mywelljourney.png" alt="day planner" />
-                        <div class="card-body"><a href="https://project-2-4.herokuapp.com/" target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block">My Wellness Journey</button></a>
+                        <div class="card-body"><a href="https://project-2-4.herokuapp.com/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-primary btn-lg btn-block">My Wellness Journey</button></a>
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
     );
 };
